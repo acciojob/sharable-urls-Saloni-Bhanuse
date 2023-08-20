315aa8999d6de0d3b29a4f8a49c621e4bbd4b390
Saloni-Bhanuse <109303739+Saloni-Bhanuse@users.noreply.github.com>
2023-08-20 11:19:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-// your code here
+
 document.getElementById("button").addEventListener("click", function() {
-    // Get the values from the input fields
+
     var nameValue = document.getElementById("name").value;
     var yearValue = document.getElementById("year").value;
 
-    // Construct the new URL with query string
     var newURL = `https://localhost:8080/?name=${encodeURIComponent(nameValue)}&year=${encodeURIComponent(yearValue)}`;
 
-    // Update the h3 text
     document.getElementById("url").textContent = newURL;
 });
