588654f16d72f7666a42881cd2d4b08fdda42049
Saloni-Bhanuse <109303739+Saloni-Bhanuse@users.noreply.github.com>
2023-08-20 11:49:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 
 document.getElementById("button").addEventListener("click", function() {
-
     var nameValue = document.getElementById("name").value;
     var yearValue = document.getElementById("year").value;
-
     var newURL = `https://localhost:8080/?name=${encodeURIComponent(nameValue)}&year=${encodeURIComponent(yearValue)}`;
 
     document.getElementById("url").textContent = newURL;
